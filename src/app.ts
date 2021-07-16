ad3939a6a3adcef2138a1d457dd1c71dd8f8ccb2
tonKristall <77803597+tonKristall@users.noreply.github.com>
2021-07-16 08:21:38 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,18 @@ import categories from './category/router';
 import cors from 'cors';
 
 const app = express();
-const PORT = process.env.PORT || 3211;
-var cloudinary = require('cloudinary').v2;
+const PORT = process.env.PORT || 3000;
+// var cloudinary = require('cloudinary').v2;
 
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 app.use('/', categories);
 app.listen(PORT, () => console.log('server'));
 
