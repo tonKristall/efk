0f10e60995624b3903aa88ed49f90911cec3a5c6
tonKristall <77803597+tonKristall@users.noreply.github.com>
2021-07-16 09:08:51 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,13 @@ const PORT = process.env.PORT || 3211;
 var cloudinary = require('cloudinary').v2;
 
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 app.use('/', categories);
 app.listen(PORT, () => console.log('server'));
 
